89196e3e462e1da6b9dd512af293c137c91c8723
Doan Bang Tran <doantran@hotmail.co.uk>
2023-04-06 13:38:19 -0700

    added reset button on profilecard

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -16,9 +16,15 @@ const ProfileCard = () => {
             <Image src={user.picture} alt={user.name} />
             <Name> {user.nickname}</Name>
             <Div>
-              <Goal>Current goal:<Bold>{Number} activities</Bold> </Goal>
-              <Sucess>Activities left to hit your target goal:<Bold> {Number} activities</Bold></Sucess>
+              <Goal>
+                Current goal:<Bold>{Number} activities</Bold>{" "}
+              </Goal>
+              <Sucess>
+                Activities left to hit your target goal:
+                <Bold> {Number} activities</Bold>
+              </Sucess>
             </Div>
+            <Reset>Reset goal</Reset>
           </InfoContainer>
         </Wrapper>
       ) : (
@@ -27,21 +33,30 @@ const ProfileCard = () => {
     </>
   );
 };
+const Reset = styled.button`
+  margin-top: 150px;
+  width: 100px;
+  text-decoration: none;
+  color: white;
+  border-radius: 10px;
+  padding: 4px;
+  background-color: #c83349;
+  border: none;
+  &:hover {
+    filter: brightness(85%);
+  }
+`;
+
 const Bold = styled.span`
-  font-weight:bold;
-  margin-left:4px;
+  font-weight: bold;
+  margin-left: 4px;
 `;
 const Div = styled.div`
-  display:flex;
-  flex-direction:column;
+  display: flex;
+  flex-direction: column;
 `;
-const Sucess = styled.div`
-  
-`;
-const Goal = styled.div`
- 
-  
-`;
+const Sucess = styled.div``;
+const Goal = styled.div``;
 const InfoContainer = styled.div`
   margin-top: 50px;
   border: solid 2px black;
@@ -60,7 +75,7 @@ const InfoContainer = styled.div`
 const Name = styled.div`
   font-size: 25px;
   font-weight: bold;
-  margin-bottom:10px;
+  margin-bottom: 10px;
 `;
 
 const Image = styled.img`
